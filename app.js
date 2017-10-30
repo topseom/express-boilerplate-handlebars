c0a943c5d6d564bf395d9c8ca423713daef7c1d7
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-10-30 14:45:07 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname+"/public"));
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home",{
+        pageTitle:"Home",
+        WelcomeMessage:"Welcome !"
+    })
+});
+
+
+app.get("/contact",(req,res)=>{
+    res.render('page',{
+        pageTitle:"Contact",
+        content:"Hello contact"
+    })
+});
+
+app.get("/about",(req,res)=>{
+    res.render('page',{
+        pageTitle:"About",
+        content:"Hello About!"
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("Server run at localhost:3000");
+});
